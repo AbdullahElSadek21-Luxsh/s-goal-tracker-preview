2030e70ae3a738a2417cda0cfbb3e55861977d55
AbdullahElSadek21-Luxsh <projects1@luxshtech.com>
2026-09-08 11:31:08 +0100

    Shorten collapsed cards: goals 54px, tasks 60px, 8px gaps

---
+++ assets/MailOutline-AhHoApZr.js
@@ -0,0 +1,1 @@
+import{j as c}from"./index-yeTQRJC1.js";import{c as o}from"./assetUrl-Ciul2hF1.js";const r=o(c.jsx("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"}),"ArrowBack"),s=o(c.jsx("path",{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 14H4V8l8 5 8-5zm-8-7L4 6h16z"}),"MailOutline");export{r as A,s as M};
